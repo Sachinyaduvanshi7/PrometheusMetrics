06a6755068141edd9b866a46178c7272fa3d516a
Sachinyaduvanshi7 <sachinyrao7@gmail.com>
2024-05-02 16:12:14 +0530

    new

---
+++ Controllers/demoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Prometheus;
+
+namespace PrometheusMetrics.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class demoController : ControllerBase
+    {
+        Counter counter = Metrics.CreateCounter("demo_counter", "index page counter");
+
+        [HttpGet]
+
+        public string Get()
+        {
+            counter.Inc();
+            return "hello";
+
+        }
+    }
+}
